200612ab6d9911a37a12d1ade3a572ca17e73610
tasainayasai <taichi49763@gmail.com>
2025-05-14 18:18:09 +0900

    Update a.js

--- a.js
+++ a.js
@@ -1,24 +1,25 @@
-const btn = document.querySelector(".btn");
-let count = 0;
-let counter=0;
-const newitem = document.querySelector(".number");
-const countt = document.querySelector(".counter");
-const sachi = ["さ","さあ","さあち","さあちゃ","さあちゃん"];
-const audio = document.querySelector("#myaudio");
+document.addEventListener("DOMContentLoaded", () => {
+　const btn = document.querySelector(".btn");
+　let count = 0;
+　let counter=0;
+　const newitem = document.querySelector(".number");
+　const countt = document.querySelector(".counter");
+　const sachi = ["さ","さあ","さあち","さあちゃ","さあちゃん"];
+　const audio = document.querySelector("#myaudio");
 
-btn.addEventListener("click", () => {
-    audio.play();
-    count += 1;
-    if (count <= 5){
-        newitem.textContent=sachi[count -1];
-    }else if (count === 6){
-        count = 0;
-        newitem.textContent=""
-    }
+　btn.addEventListener("click", () => {
+    　audio.play();
+    　count += 1;
+    　if (count <= 5){
+        　newitem.textContent=sachi[count -1];
+    　}else if (count === 6){
+        　count = 0;
+        　newitem.textContent=""
+    　}
 
-    if (count === 5){
-        counter++;
-    }
-    countt.textContent=counter;
+    　if (count === 5){
+        　counter++;
+    　}
+    　countt.textContent=counter;
+　});
 });
-
